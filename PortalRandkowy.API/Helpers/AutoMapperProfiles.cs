f654df746b7883bfad680f2cd23755a77032225c
wisnia10236 <56563777+wisnia10236@users.noreply.github.com>
2020-04-12 12:29:42 +0200

    konf dokł automappera , obliczanie wieku i url

--- PortalRandkowy.API/Helpers/AutoMapperProfiles.cs
+++ PortalRandkowy.API/Helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using PortalRandkowy.API.Dtos;
 using PortalRandkowy.API.Models;
@@ -8,8 +9,26 @@ namespace PortalRandkowy.API.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<User, UserForListDto>(); // utworz mappera z listy User do UserForListDto
-            CreateMap<User, UserForDetailedDto>();
+            CreateMap<User, UserForListDto>()   // utworz mappera z listy User do UserForListDto
+                    .ForMember(dest => dest.PhotoUrl, opt =>    // ustawiamy  ze ma wziac PhotoUrl z Photo dla DTO
+                    {
+                        opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);    // przekazujemy ze ma wziac glowne zdj z photo i wpisujemy ze to wskazanie url
+                    })
+                    .ForMember(dest => dest.Age, opt =>
+                    {
+                        opt.MapFrom(src => src.DateOfBirth.CalculateAge());             // stworzona klasa aby liczyla wiek
+                    });
+            CreateMap<User, UserForDetailedDto>()
+                    .ForMember(dest => dest.PhotoUrl, opt =>
+                    {
+                        opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                    })
+                    .ForMember(dest => dest.Age, opt =>
+                    {
+                        opt.MapFrom(src => src.DateOfBirth.CalculateAge());             // stworzona klasa aby liczyla wiek
+                    });
+            CreateMap<Photo, PhotosForDetailedDto>();
         }
+
     }
 }
---
+++ PortalRandkowy.API/Helpers/Extensions.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace PortalRandkowy.API.Helpers
+{
+    public static class Extensions
+    {
+        public static int CalculateAge(this DateTime datetime)
+        {
+            var age = DateTime.Today.Year - datetime.Year;
+
+            if (datetime.AddYears(age) > DateTime.Today)
+                age--;
+            return age;
+        }
+    }
+}
