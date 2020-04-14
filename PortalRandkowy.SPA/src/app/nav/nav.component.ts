14666118918fb75b107f0efe956e04bebd628000
wisnia10236 <56563777+wisnia10236@users.noreply.github.com>
2020-04-15 00:52:59 +0200

    przekierowanie do wybranej strony

--- PortalRandkowy.SPA/src/app/nav/nav.component.ts
+++ PortalRandkowy.SPA/src/app/nav/nav.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
-import { AlertifyService } from '../_services/alertify.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../_services/auth.service";
+import { AlertifyService } from "../_services/alertify.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"],
 })
 export class NavComponent implements OnInit {
-
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login() {
-    this.authService.login(this.model).subscribe(next => {
-      this.alertify.success('Zalogowałeś sie do aplikacji');
-    }, error => {
-      this.alertify.error('Wystąpił błąd logowania');
-    });
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        this.alertify.success("Zalogowałeś sie do aplikacji");
+      },
+      (error) => {
+        this.alertify.error("Wystąpił błąd logowania");
+      },
+      () => {
+        // deklarujemy niewiadoma zmienna aby po akceptacji wywolac ja czyli nawigowac po kliknieciu do strony /uzytkownicy
+        this.router.navigate(["/uzytkownicy"]);
+      }
+    );
   }
 
   loggedIn() {
@@ -29,8 +39,8 @@ export class NavComponent implements OnInit {
   }
 
   logout() {
-    localStorage.removeItem('token');
-    this.alertify.message('Zostałeś wylogowany');
+    localStorage.removeItem("token");
+    this.alertify.message("Zostałeś wylogowany");
+    this.router.navigate(["/home"]); // po wylogowaniu nawigujemy strone do /home
   }
-
 }
