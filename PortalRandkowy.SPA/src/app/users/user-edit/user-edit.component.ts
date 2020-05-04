8d435a610a7e67a44c04cb900de2e16b766b6c4e
wisnia10236 <56563777+wisnia10236@users.noreply.github.com>
2020-05-04 10:05:56 +0200

    ulepszenie szablonu edycji uzytk

--- PortalRandkowy.SPA/src/app/users/user-edit/user-edit.component.ts
+++ PortalRandkowy.SPA/src/app/users/user-edit/user-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-edit',
@@ -11,12 +13,20 @@ export class UserEditComponent implements OnInit {
 
   user:User;
 
-  constructor(private route: ActivatedRoute ) { }
+  @ViewChild('editForm',{static: false}) editForm: NgForm;
+
+  constructor(private route: ActivatedRoute ,private alerify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data =>{
       this.user = data.user;
     });
+  }
+
+  updateUser(){
+    console.log(this.user);
+    this.alerify.success('Profil pomyślnie zaktualizowany');
+    this.editForm.reset(this.user);
   }
 
 }
