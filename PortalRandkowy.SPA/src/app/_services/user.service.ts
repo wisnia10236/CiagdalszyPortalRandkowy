2a8c0168cf5fdb5e270b62f17da2a4ea8212381a
wisnia10236 <56563777+wisnia10236@users.noreply.github.com>
2020-05-24 16:34:39 +0200

    edycja na stronie

--- PortalRandkowy.SPA/src/app/_services/user.service.ts
+++ PortalRandkowy.SPA/src/app/_services/user.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { User } from "../_models/user";
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class UserService {
   baseUrl = environment.apiUrl;
@@ -14,10 +14,16 @@ export class UserService {
 
   getUsers(): Observable<User[]> {
     /* pobieranie userow z bazy */
-    return this.http.get<User[]>(this.baseUrl + "users");
+    return this.http.get<User[]>(this.baseUrl + 'users');
   }
 
   getUser(id: number): Observable<User> {
-    return this.http.get<User>(this.baseUrl + "users/" + id);
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
+
+  updateUser(id: number, user: User)
+  {
+    return this.http.put(this.baseUrl + 'users/' + id , user);   // przekazyjemy dla adresu ... zmiany z api
+  }
+
 }
--- PortalRandkowy.SPA/src/app/users/user-edit/user-edit.component.ts
+++ PortalRandkowy.SPA/src/app/users/user-edit/user-edit.component.ts
@@ -3,6 +3,8 @@ import { User } from 'src/app/_models/user';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -11,18 +13,22 @@ import { NgForm } from '@angular/forms';
 })
 export class UserEditComponent implements OnInit {
 
-  user:User;
+  user: User;
 
-  @ViewChild('editForm',{static: false}) editForm: NgForm;
-  @HostListener('window:beforeunload',['$event'])
-  uploadNotification($event:any){
-    if(this.editForm.dirty)
+  @ViewChild('editForm', {static: false}) editForm: NgForm;
+  @HostListener('window:beforeunload', ['$event'])
+  uploadNotification($event: any)
+  {
+    if (this.editForm.dirty)
     {
-      $event.returnValue= true;
+      $event.returnValue = true;
     }
   }
 
-  constructor(private route: ActivatedRoute , private alerify: AlertifyService) { }
+  constructor(private route: ActivatedRoute ,
+              private alerify: AlertifyService,
+              private userService: UserService ,
+              private authService: AuthService ) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -31,9 +37,17 @@ export class UserEditComponent implements OnInit {
   }
 
   updateUser(){
-    console.log(this.user);
-    this.alerify.success('Profil pomyślnie zaktualizowany');
-    this.editForm.reset(this.user);
+
+    // tslint:disable-next-line:max-line-length
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user)         // odwolujemy sie do serwisu z parametramy z id oraz user aby mogl zupdatowac zmiany na bazie dancyh
+          .subscribe(next => {            // jest to zmiana na bazie danych i jezeli wszystko bedzie ok to wyswietla alerify
+                 this.alerify.success('Profil pomyślnie zaktualizowany');
+                 this.editForm.reset(this.user);
+                  }, error => {
+                    this.alerify.error(error);
+                  });
+
+
   }
 
 }
