f29b1b8a51666558e7e8c296100a1ddaf15f6005
wisnia10236 <56563777+wisnia10236@users.noreply.github.com>
2020-06-23 00:29:31 +0200

    Komponent dla zdjeć

--- PortalRandkowy.SPA/src/app/app.module.ts
+++ PortalRandkowy.SPA/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { UserListResolver } from './_resolvers/user-list.resolver';
 import { UserEditComponent } from './users/user-edit/user-edit.component';
 import { UserEditResolver } from './_resolvers/user-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { PhotosComponent } from './users/photos/photos.component';
 
 
 export function tokenGetter() {
@@ -56,6 +57,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
     UserCardComponent,
     UserDetailComponent,
     UserEditComponent,
+    PhotosComponent,
   ],
   imports: [
     BrowserModule,
