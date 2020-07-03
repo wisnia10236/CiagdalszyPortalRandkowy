9cef1a3c9e03bd73d9e5619c361b59513a9bada0
wisnia10236 <56563777+wisnia10236@users.noreply.github.com>
2020-07-03 09:37:43 +0200

    Zmiana farmularza rejestr na reactiveForms

--- PortalRandkowy.SPA/src/app/app.module.ts
+++ PortalRandkowy.SPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JwtModule } from '@auth0/angular-jwt';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -78,6 +78,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
     NgxGalleryModule,
     [RouterModule.forChild(appRoutes)],
     FileUploadModule,
+    ReactiveFormsModule
   ],
   providers: [
     AuthService,
--- PortalRandkowy.SPA/src/app/register/register.component.ts
+++ PortalRandkowy.SPA/src/app/register/register.component.ts
@@ -1,32 +1,41 @@
-import { Component, OnInit, Output, EventEmitter } from "@angular/core";
-import { AuthService } from "../_services/auth.service";
-import { AlertifyService } from "../_services/alertify.service";
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: "app-register",
-  templateUrl: "./register.component.html",
-  styleUrls: ["./register.component.css"],
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(
     private authService: AuthService,
     private alertify: AlertifyService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    });
+  }
 
   register() {
-    this.authService.register(this.model).subscribe(
-      () => {
-        this.alertify.success("rejestracja udana");
-      },
-      (error) => {
-        this.alertify.error(error);
-      }
-    );
+    // this.authService.register(this.model).subscribe(
+    //   () => {
+    //     this.alertify.success('rejestracja udana');
+    //   },
+    //   (error) => {
+    //     this.alertify.error(error);
+    //   }
+    // );
+    console.log(this.registerForm.value);
   }
 
   cancel() {
