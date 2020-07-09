93b5b3461f841cf2e6ee199698a33f5bac8fefe9
wisnia10236 <56563777+wisnia10236@users.noreply.github.com>
2020-07-09 23:22:09 +0200

    Dokończenie rejestracji w SPA(koncowa wersja)

--- PortalRandkowy.SPA/src/app/register/register.component.ts
+++ PortalRandkowy.SPA/src/app/register/register.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +13,7 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
@@ -19,6 +21,7 @@ export class RegisterComponent implements OnInit {
     private authService: AuthService,
     private alertify: AlertifyService,
     private fb: FormBuilder,
+    private router: Router,
   ) {}
 
   ngOnInit() {
@@ -49,15 +52,21 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    // this.authService.register(this.model).subscribe(
-    //   () => {
-    //     this.alertify.success('rejestracja udana');
-    //   },
-    //   (error) => {
-    //     this.alertify.error(error);
-    //   }
-    // );
-    console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+
+      this.user = Object.assign({}, this.registerForm.value );    // z formularza przypisujemy wartosci do usera
+
+      this.authService.register(this.user).subscribe(() => {
+        this.alertify.success('rejestracja udana');
+      },
+      (error) => {
+        this.alertify.error(error);
+      }, () => {            // jesli zarejestruje nas to subskrybujemy uztk i przerzucamy go do strony  automatycznie z uzytkownikam,i
+        this.authService.login(this.user).subscribe(() => {
+          this.router.navigate(['/uzytkownicy']);
+        });
+      });
+    }
   }
 
   cancel() {
